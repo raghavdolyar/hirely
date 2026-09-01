91f68477d28b0f610fdc6884fa8d4232d10b28d8
raghavdolyar <raghavdolyardell@gmail.com>
2026-09-01 20:34:02 +0530

    added cors package

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }),
+);
 
 // importing all the routes
 import authRouter from './routes/auth.routes.js';
